4f35d2d7867070872eba5959b7dff547a917e152
Burcu Kamilcelebi <burcucelebi33@gmail.com>
2023-08-28 12:08:50 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Burcu Kamilçelebi on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
